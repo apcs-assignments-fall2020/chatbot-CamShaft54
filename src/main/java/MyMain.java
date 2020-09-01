840adff3c34d2ec1b97e1e8588aeba5a375f047f
CamShaft54 <cam.shaw1234@gmail.com>
2020-09-01 17:18:53 +0000

    Make Chatbot

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hi, " + name + "!" + "\nWhat is your favorite color, " + name + "?");
+        String color = scan.next();
+        System.out.println("I like the color " + color + " too!" + "\nWhat is your Social Security Number?");
+        String SSN = scan.next();
+        System.out.println("MUHAHAHAHA NOW I KNOW YOUR SOCIAL SECURITY NUMBER IS " + SSN + "!");
         scan.close();
     }
 }
